8b77a2f6c593a639ce31a27ad785c0f3d276a2f7
Ruto <ruto@protonmail.ch>
2020-10-27 00:13:04 -0400

    Some sidebar fun

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,10 +1,19 @@
 <template>
   <aside
-    class="transform top-0 left-0 w-64 bg-gray-600 absolute h-full overflow-auto ease-in-out transition-all duration-200"
+    class="flex flex-col flex-shrink-0 transform top-0 left-0 w-64 bg-gray-600 absolute h-full overflow-auto ease-in-out transition-all duration-200"
     :class="value ? 'translate-x-0' : '-translate-x-full'"
   >
     <div class="h-16" />
-    <a href="#">test</a>
+    <div class="flex flex-row items-center justify-between flex-shrink-0 px-8 py-4">
+      <a href="#" class="text-lg font-semibold tracking-widest uppercase">GlobalAPI Portal</a>
+    </div>
+    <hr/>
+    <nav class="flex-grow block pb-0 overflow-y-auto">
+      <n-link
+        v-for="nav of navItems"
+        :key="nav.name"
+        class="block px-4 py-2 hover:bg-red-600" :to="nav.path">{{ nav.name }}</n-link>
+    </nav>
   </aside>
 </template>
 
@@ -16,6 +25,28 @@ export default Vue.extend({
     value: {
       type: Boolean,
       required: true
+    }
+  },
+  data() {
+    return {
+      navItems: [
+        {
+          path: '#maps',
+          name: 'Maps'
+        },
+        {
+          path: '#servers',
+          name: 'Servers'
+        },
+        {
+          path: '#records',
+          name: 'Records'
+        },
+        {
+          path: '#bans',
+          name: 'Bans'
+        },
+      ]
     }
   }
 });
