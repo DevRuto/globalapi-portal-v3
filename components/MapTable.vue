0571a40812439eddafd943100ccdbfd72b767133
Ruto <ruto@protonmail.ch>
2020-12-16 21:05:05 -0500

    Global filtering

--- components/MapTable.vue
+++ components/MapTable.vue
@@ -1,24 +1,28 @@
 <template>
   <div class="min-w-screen overflow-x-visible">
     <div class="table-fixed text-black dark:text-white mx-auto my-4">
-      <div class="flex justify-end mx-auto text-white my-2 h-6">
-        <label for="count" class="text-black dark:text-white text-sm">Per page</label>
-        <select
-          id="count"
-          v-model="count"
-          class="text-black dark:text-white dark:bg-black border border-gray-600 rounded px-2 mx-2"
-          @change="saveCount"
-        >
-          <option v-for="option in countOptions" :key="option.value" :value="option.value">{{ option.text }}</option>
-        </select>
-        <button type="button" class="px-2 mx-2 rounded border border-gray-800 dark:border-white text-black dark:text-white focus:outline-none hover:bg-gray-400 dark:hover:bg-gray-700" @click="prevPage">
-          Back
-        </button>
-        <button type="button" class="px-2 mx-2 rounded border border-gray-800 dark:border-white text-black dark:text-white focus:outline-none hover:bg-gray-400 dark:hover:bg-gray-700" @click="nextPage">
-          Next
-        </button>
+      <div class="flex mx-auto text-white my-2 h-6 justify-end">
+        <div class="inline-flex">
+          <label for="count" class="text-black dark:text-white text-sm">Per page</label>
+          <select
+            id="count"
+            v-model="count"
+            class="text-black dark:text-white dark:bg-black rounded-full count"
+            @change="saveCount"
+          >
+            <option v-for="option in countOptions" :key="option.value" :value="option.value">
+              {{ option.text }}
+            </option>
+          </select>
+          <button type="button" class="px-4 mx-2 rounded border border-gray-800 dark:border-white text-black dark:text-white focus:outline-none hover:bg-gray-400 dark:hover:bg-gray-700" @click="prevPage">
+            Back
+          </button>
+          <button type="button" class="px-4 mx-2 rounded border border-gray-800 dark:border-white text-black dark:text-white focus:outline-none hover:bg-gray-400 dark:hover:bg-gray-700" @click="nextPage">
+            Next
+          </button>
+        </div>
       </div>
-      <div class="flex flex-col w-full border border-gray-600">
+      <div class="flex flex-col w-full border-2 border-gray-600 rounded-md">
         <div class="hidden md:flex flex-col md:flex-row cursor-pointer select-none">
           <span class="w-1/12 px-2 py-1 flex" @click="sortTable('id')">
             ID
@@ -60,7 +64,7 @@
           <div
             v-for="(map, i) of paginatedValue"
             :key="i"
-            class="flex flex-col md:flex-row hover:bg-purple-200 dark:hover:bg-purple-800"
+            class="flex flex-col transition ease-in-out duration-75 md:flex-row hover:bg-purple-200 dark:hover:bg-purple-800"
             :class="i % 2 == 0 ? 'bg-gray-200 dark:bg-gray-800' : 'bg-gray-100 dark:bg-gray-900'"
           >
             <span class="md:w-1/12 px-2 py-1 flex"><span class="md:hidden font-bold w-1/3">ID: </span>{{ map.id }}</span>
@@ -69,7 +73,7 @@
             <span class="md:w-2/12 px-2 py-1 flex"><span class="md:hidden font-bold w-1/3">Difficulty: </span>{{ map.difficulty }} - {{ getDifficultyText(map.difficulty) }}</span>
             <span class="md:w-1/12 px-2 py-1 flex"><span class="md:hidden font-bold w-1/3">Global: </span>{{ map.validated }}</span>
             <span class="md:w-auto px-2 py-1 flex"><span class="md:hidden font-bold w-1/3">Created: </span>{{ map.created_on }}</span>
-            <span class="md:w-1/12 px-2 py-1 text-center text-blue-400"><n-link to="#">Edit</n-link></span>
+            <span class="md:w-1/12 px-2 py-1 text-right text-blue-400"><n-link to="#">Edit</n-link></span>
           </div>
         </div>
       </div>
@@ -78,10 +82,12 @@
         <select
           id="count"
           v-model="count"
-          class="text-black dark:text-white dark:bg-black border border-gray-600 rounded px-2 mx-2"
+          class="text-black dark:text-white dark:bg-black border border-gray-600 rounded px-2 mx-2 count"
           @change="saveCount"
         >
-          <option v-for="option in countOptions" :key="option.value" :value="option.value">{{ option.text }}</option>
+          <option v-for="option in countOptions" :key="option.value" :value="option.value">
+            {{ option.text }}
+          </option>
         </select>
         <button type="button" class="px-2 mx-2 rounded border border-gray-800 dark:border-white text-black dark:text-white hover:bg-gray-400 dark:hover:bg-gray-700" @click="prevPage">
           Back
@@ -132,11 +138,11 @@ export default Vue.extend({
   computed: {
     paginatedValue (): any[] {
       console.log('[Maps pagination] Start: %i, Count: %i', this.start, this.count);
-      let maps = [...this.value];
-      if (!!this.sort) {
+      const maps = [...this.value];
+      if (this.sort) {
         const index = this.sort.indexOf(':');
         const col = this.sort.substring(0, index);
-        const direction = this.sort.substring(index + 1) == 'asc' ? 1 : -1;
+        const direction = this.sort.substring(index + 1) === 'asc' ? 1 : -1;
         maps.sort((a: any, b: any) => {
           const varType = typeof a[col];
           let retVal = 0;
@@ -159,11 +165,11 @@ export default Vue.extend({
   methods: {
     nextPage () {
       this.start = Math.min(this.start + this.count, this.value.length);
-      window.scrollTo(0,0);
+      window.scrollTo(0, 0);
     },
     prevPage () {
       this.start = Math.max(this.start - this.count, 0);
-      window.scrollTo(0,0);
+      window.scrollTo(0, 0);
     },
     getDifficultyText (difficulty: number) {
       return difficultyLabels[difficulty - 1];
@@ -171,9 +177,9 @@ export default Vue.extend({
     saveCount () {
       localStorage.setItem('mapcount', this.count.toString());
     },
-    sortTable(col: string) {
+    sortTable (col: string) {
       console.log(`${this.sort} - ${col}`);
-      if (this.sort.substring(0, this.sort.indexOf(':')) == col) {
+      if (this.sort.substring(0, this.sort.indexOf(':')) === col) {
         if (this.sort.indexOf(':asc') > 0) {
           this.sort = `${col}:desc`;
         } else {
@@ -186,3 +192,14 @@ export default Vue.extend({
   }
 });
 </script>
+
+<style scoped>
+/*
+  default select css from
+  https://github.com/tailwindlabs/tailwindcss-forms
+  seems to have too much padding
+*/
+.count {
+  padding-top: 0px !important;
+}
+</style>
--- pages/dashboard/maps/index.vue
+++ pages/dashboard/maps/index.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <form class="text-black dark:text-white mx-auto w-2/3 py-2" @change="updateFilters">
+      <div>
+        <input v-model="global" type="checkbox" name="global" class="rounded border-gray-300 text-purple-600 focus:border-purple-300 focus:ring focus:ring-purple-200 focus:ring-opacity-50">
+        <label for="global">Global</label>
+      </div>
+    </form>
+    <hr class="mx-auto px-2 w-full lg:w-2/3 border-gray-600">
     <map-table v-model="maps" class="table mx-auto md:w-full" />
   </div>
 </template>
@@ -15,7 +22,8 @@ export default Vue.extend({
   },
   data () {
     return {
-      maps: []
+      maps: [],
+      global: true
     };
   },
   mounted () {
@@ -24,12 +32,15 @@ export default Vue.extend({
   methods: {
     async loadMaps () {
       const params = new URLSearchParams();
-      params.set('is_validated', 'true');
+      params.set('is_validated', this.global.toString());
       params.set('offset', '0');
       params.set('limit', '9999');
       const url = `${API_HOST}/api/v2.0/maps`;
       this.maps = await this.$axios.$get(url, { params });
       console.log(this.maps);
+    },
+    updateFilters () {
+      this.loadMaps();
     }
   }
 });
