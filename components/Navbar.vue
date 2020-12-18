048040612bb349bd05f5e2c56e8de8a44c6bdc9a
Ruto <ruto@protonmail.ch>
2020-12-17 19:32:54 -0500

    Toggle component

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="flex items-center justify-between h-16 px-6 z-40 bg-gray-800 text-white">
+  <nav class="flex items-center justify-between h-16 px-6 z-40 bg-gray-800 text-white top-0">
     <div class="flex items-center">
       <button type="button" class="lg:hidden block text-gray-500 hover:text-white focus:text-white focus:outline-none" @click="$emit('sidebar-toggle')">
         <Hamburger v-model="value" />
@@ -7,9 +7,8 @@
       <span class="text-white text-2xl font-semibold tracking-widest pl-2 mx-auto">GlobalAPI</span>
     </div>
     <div class="flex items-center">
-      <button type="button" class="block text-white border rounded bg-gray-800 px-4 mr-4" @click="toggleTheme">
-        Theme
-      </button>
+      <label>Theme</label>
+      <toggle v-model="darkTheme" class="px-4" @click="toggleTheme" />
       <button type="button" class="block text-gray-500 hover:text-white focus:text-white focus:outline-none">
         Login
       </button>
@@ -19,11 +18,13 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import Toggle from '@/components/Toggle.vue';
 import Hamburger from '~/components/Hamburger.vue';
 
 // https://github.com/tailwindlabs/designing-with-tailwindcss/tree/master/03-building-a-responsive-navbar/03-making-the-navbar-responsive
 export default Vue.extend({
   components: {
+    Toggle,
     Hamburger
   },
   props: {
@@ -34,12 +35,14 @@ export default Vue.extend({
   },
   data () {
     return {
-      isOpen: false
+      isOpen: false,
+      darkTheme: false
     };
   },
   mounted () {
     if (localStorage.theme === 'dark') {
       document?.querySelector('html')?.classList.add('dark');
+      this.darkTheme = true;
     }
   },
   methods: {
@@ -48,9 +51,11 @@ export default Vue.extend({
       if (localStorage.theme === 'dark') {
         document?.querySelector('html')?.classList.remove('dark');
         localStorage.removeItem('theme');
+        this.darkTheme = false;
       } else {
         document?.querySelector('html')?.classList.add('dark');
         localStorage.setItem('theme', 'dark');
+        this.darkTheme = true;
       }
     }
   }
---
+++ components/Toggle.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex relative transition duration-200 ease-in overflow-hidden" @click="toggleClick">
+    <input type="checkbox" class="hidden" @change="$emit('click', value)">
+    <div class="flex cursor-pointer my-auto bg-gray-200 dark:bg-gray-700 w-10 h-5 px-1 rounded-full">
+      <span class="block my-auto w-4 h-4 my-auto border border-gray-400 dark:border-gray-600 rounded-full transition transform" :class="value ? 'bg-gray-800 translate-x-full' : 'bg-gray-100'" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  props: {
+    value: {
+      type: Boolean,
+      required: true
+    }
+  },
+  methods: {
+    toggleClick () {
+      this.$emit('click', !this.value);
+    }
+  }
+});
+</script>
