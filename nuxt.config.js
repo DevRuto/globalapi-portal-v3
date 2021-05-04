8b8a626b4f63efa63de3346b1957adb25c8a6db6
Ruto <ruto@protonmail.ch>
2021-05-03 21:44:32 -0400

    Add repository path for nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/globalapi-portal-v3/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
