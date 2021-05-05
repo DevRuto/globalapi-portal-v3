22a0a95c1a93b0d21c4fb3b6088027f570306f9c
DevRuto <20764435+DevRuto@users.noreply.github.com>
2021-05-05 21:55:06 +0000

    Deploying to gh-pages from @ DevRuto/globalapi-portal-v3@1ae96678ed461f573b38ce0e9879186f93777198 🚀

---
+++ _nuxt/0964fde.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{1199:function(e,n,t){"use strict";t.r(n);var r=t(84),component=Object(r.a)({},(function(){var e=this,n=e.$createElement;return(e._self._c||n)("div",[e._v("Owned Servers")])}),[],!1,null,null,null);n.default=component.exports}}]);
